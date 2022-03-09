4ceb845595bc263fe04d4ec2a82802a0bf3560d9
이승윤 <yyuunn17@naver.com>
2022-03-09 20:35:02 +0900

    ⚙️ :: HelloWorld.vue 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.svg">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="PORRONG logo" src="./assets/logo.svg">
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
