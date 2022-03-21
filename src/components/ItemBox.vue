29b0caaefaec4dc96cc30ff14bb4e76b9c369f86
이승윤 <yyuunn17@naver.com>
2022-03-21 19:52:51 +0900

    📑 :: 메인 페이지 퍼블리싱

---
+++ src/components/ItemBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="item_box" />
+</template>
+
+<script>
+</script>
+
+<style>
+.item_box {
+  width: 240px;
+  height: 120px;
+
+  background: rgba(255, 213, 3, 0.2);
+  border-radius: 10px;
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <div id="logo">
+      <img src="../assets/logo.svg" />
+    </div>
+    <div id="withdrawal">
+      <p>탈퇴하기</p>
+    </div>
+    <div class="itemContainer">
+      <ItemBox v-for="data in 10" :key="data" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemBox from "../components/ItemBox.vue";
+
+export default {
+  components: {
+    ItemBox,
+  },
+};
+</script>
+
+<style>
+.container {
+  position: absolute;
+  width: 765px;
+  height: 532px;
+}
+#logo {
+  margin: 0 auto;
+  width: 56px;
+}
+#withdrawal {
+  float: right;
+  font-family: "Roboto";
+  font-size: 11px;
+  line-height: 12px;
+  text-decoration-line: underline;
+
+  color: rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+}
+
+.itemContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 440px;
+  gap: 15px;
+  overflow: auto;
+}
+.itemContainer::-webkit-scrollbar {
+  width: 5px; /* 스크롤바의 너비 */
+}
+
+.itemContainer::-webkit-scrollbar-thumb {
+  height: 30%; /* 스크롤바의 길이 */
+  background: #fab000; /* 스크롤바의 색상 */
+
+  border-radius: 10px;
+}
+
+.itemContainer::-webkit-scrollbar-track {
+  background: rgba(33, 122, 244, 0.1); /*스크롤바 뒷 배경 색상*/
+  border-radius: 10px;
+}
+</style>
