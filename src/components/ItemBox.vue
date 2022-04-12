6369612736a92d3b1fb21d904bf8723945af062c
이승윤 <yyuunn17@naver.com>
2022-04-12 13:27:59 +0900

    ⚡️ :: 작성박스 아이템 추가

--- src/components/ItemBox.vue
+++ src/components/ItemBox.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="item_box" @click.prevent="SET_OPEN_MODAL(true)"/>
+  <div class="item_box" @click.prevent="SET_OPEN_MODAL(true)">
+    <div class="image">
+      <img src="../assets/letterpaper.svg"/>
+    </div>
+    <p class="write">작성하기</p>
+  </div>
 </template>
 
 <script>
@@ -13,11 +18,35 @@ import { mapMutations } from 'vuex'
 
 <style>
 .item_box {
+  position: relative;
   width: 240px;
   height: 120px;
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: rgba(255, 213, 3, 0.2);
   border-radius: 10px;
   cursor: pointer;
+}
+.image {
+  margin: 0 auto;
+  width: 96px;
+  height: 84px;
+  left: 385.54px;
+  top: 197px;
+  position: absolute;
+  left: 60px;
+  top: 0;
+
+  opacity: 0.3;
+  transform: rotate(15.57deg);
+}
+.write {
+  font-family: 'Noto Sans KR';
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+
+  color: rgba(119, 74, 0, 0.8);
 }
 </style>
