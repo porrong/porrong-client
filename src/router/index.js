0e88210b5feb546e0f81b8b33fad59c71ba77940
이승윤 <yyuunn17@naver.com>
2022-04-28 21:08:26 +0900

    ⚡️ :: LoginSuccessPage 추가 - 후처리는 조금 더 해주기

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createWebHistory, createRouter } from "vue-router";
 import LoginPage from '../views/LoginPage.vue'
+import LoginSuccessPage from '../views/LoginSuccessPage.vue'
 import MainPage from '../views/MainPage.vue'
 
 const routes = [
   {
     path: "/",
     component: LoginPage
+  },
+  {
+    path: "/login",
+    component: LoginSuccessPage
   },
   {
     path: "/main",
