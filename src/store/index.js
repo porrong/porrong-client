2820105a9d2b2b5debd07df9822f1134ab8f1ff0
이승윤 <yyuunn17@naver.com>
2022-04-28 21:09:30 +0900

    ⚡️ :: 곧 쓸 것 같은 status 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
+import router from "@/router";
 import Vuex from "vuex";
 
 const state = {
   isShowWriteLetterModal: false,
   isShowReadLetterModal: false,
+  token: ''
 };
 
 const mutations = {
@@ -12,6 +14,18 @@ const mutations = {
   SET_OPEN_READ_MODAL(state, toggle) {
     state.isShowReadLetterModal = toggle;
   },
+  LOGIN: function(state, payload) {
+    state.token = payload.token
+  },
+  LOGIN_CHECK: function(state) {
+    if(!state.token) {
+      router.push({
+        name: 'login'
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
 };
 
 const store = new Vuex.Store({
