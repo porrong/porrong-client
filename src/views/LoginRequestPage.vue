45f82491f830a08afdafa4a59466d463ce44ed60
이승윤 <yyuunn17@naver.com>
2022-04-29 11:47:47 +0900

    ⚡️ :: 카카오 로그인 성공 - 발급 받은 토큰 사용하는 로직 추가해야함

---
+++ src/views/LoginRequestPage.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from 'axios'
+import router from '@/router';
+
+export default ({
+    mounted() { //페이지가 로딩되자마자 실행되는 메소드    
+        const url = window.location.search;
+        const code = url.replace('?code=','');
+
+        axios.post('http://localhost:8989/user', {
+            "code" : code
+        })
+        .then(res => {
+            console.log(res.data);
+            router.push('/main'); //로그인에 성공한 경우 /main으로 이동
+        }).catch(error => {
+            console.log(error.message);
+            router.push('/');
+            alert('로그인에 실패하였습니다.');
+        });
+    }
+})
+</script>
