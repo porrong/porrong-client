798844a0527059dbdee21fe26713e66d44037349
이승윤 <yyuunn17@naver.com>
2022-05-19 13:44:58 +0900

    📑 :: base url 설정

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import { createApp } from "vue";
 import "./index.css";
 import router from "./router";
 import VueAxios from "vue-axios";
-import store from './store'
+import store from "./store";
 import axios from "axios";
 
 import App from "./App.vue";
+
 const app = createApp(App);
 app.use(router);
 app.use(store);
 app.use(VueAxios, axios);
 app.mount("#app");
+
+app.config.globalProperties.axios = axios;
+axios.defaults.baseURL = "http://localhost:8989";
