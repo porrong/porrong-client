bc0abc0a124b4c55551213b01d930c35d84c1e5b
이승윤 <yyuunn17@naver.com>
2022-05-19 13:45:23 +0900

    📑 :: 사용하지 않는 상태 삭제

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
-import router from "@/router";
 import Vuex from "vuex";
 
 const state = {
   isShowWriteLetterModal: false,
-  isShowReadLetterModal: false,
-  token: ''
+  isShowReadLetterModal: false
 };
 
 const mutations = {
@@ -14,18 +12,6 @@ const mutations = {
   SET_OPEN_READ_MODAL(state, toggle) {
     state.isShowReadLetterModal = toggle;
   },
-  LOGIN: function(state, payload) {
-    state.token = payload.token
-  },
-  LOGIN_CHECK: function(state) {
-    if(!state.token) {
-      router.push({
-        name: 'login'
-      }).catch(error => {
-        console.log(error)
-      })
-    }
-  }
 };
 
 const store = new Vuex.Store({
