529de1a77a1e3eb69d0ee4b58fc29eb89e196b9c
이승윤 <yyuunn17@naver.com>
2022-05-26 22:45:47 +0900

    ⚰️ :: 사용하지 않는 store 삭제

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,6 @@ import Vuex from "vuex";
 const state = {
   isShowWriteLetterModal: false,
   isShowReadLetterModal: false,
-  letters: [],
-  totalElements: 0,
 };
 
 const mutations = {
@@ -14,12 +12,6 @@ const mutations = {
   SET_OPEN_READ_MODAL(state, toggle) {
     state.isShowReadLetterModal = toggle;
   },
-  SET_TOTAL_ELEMENTS(state, data) {
-    state.totalElements = data;
-  },
-  SET_LETTERS(state, data) {
-    state.letters = data;
-  }
 };
 
 const store = new Vuex.Store({
